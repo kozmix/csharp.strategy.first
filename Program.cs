651a68fb79d3c54f6979d9d02ce89ab0cf48a2ff
Lorant Kozma <lorant.kozma@gmail.com>
2020-05-21 15:26:45 +0300

    creating the csharp project - strategy.first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strategy.First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
